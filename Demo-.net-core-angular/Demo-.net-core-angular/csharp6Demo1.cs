b66be39b1eeb5081c16a02d33f83be3e1660f647
baronhq <26222521@qq.com>
2019-02-21 11:47:08 +0800

    1

---
+++ Demo-.net-core-angular/Demo-.net-core-angular/csharp6Demo1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static System.Console;
+
+namespace Demo_.net_core_angular
+{
+    public class csharp6Demo1
+    {
+        //System.Console
+
+        public bool a() => 1 == 1;
+
+        public string b => a() + "aaa";
+    }
+
+    public class Person
+    {
+        public int age { get; set; } = 42;
+
+        //private readonly int _bookId;
+        public int BookId { get; }
+
+        public void A(object o)
+        {
+            throw new ArgumentNullException(nameof(o));
+        }
+    }
+}
