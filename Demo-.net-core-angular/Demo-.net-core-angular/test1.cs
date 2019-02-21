a17d75a4f2fe1545b8cee9ad0c25fffa240ac1ec
baronhq <26222521@qq.com>
2019-02-21 11:33:48 +0800

    1

---
+++ Demo-.net-core-angular/Demo-.net-core-angular/test1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static System.Console;
+
+namespace Demo_.net_core_angular
+{
+    public class test1
+    {
+        //System.Console
+    }
+}
